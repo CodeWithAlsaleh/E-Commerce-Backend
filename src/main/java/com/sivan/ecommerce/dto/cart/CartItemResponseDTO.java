8a60ab5673670ae7bca1a657b71b0fb92a1f2fd0
Mohammed Alsaleh <ahmedissa200016@gmail.com>
2026-06-22 02:42:18 +0300

    refactor: updated CartItemResponseDTO to reflect real world cartItem

--- src/main/java/com/sivan/ecommerce/dto/cart/CartItemResponseDTO.java
+++ src/main/java/com/sivan/ecommerce/dto/cart/CartItemResponseDTO.java
@@ -4,5 +4,7 @@ import java.util.UUID;
 
 public record CartItemResponseDTO(UUID productId,
                                   String productTitle,
+                                  long price,
+                                  boolean isActive,
                                   int quantity) {
 }
